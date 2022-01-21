23986f1cd3b2682d9fc4fca71295006c888e1795
yomZsamora <samora.y@adzumi.co.ke>
2022-01-21 08:28:48 +0300

    Completed Lab.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Binds an event listener in addingEventListener()
 function addingEventListener() {
 	const input = document.getElementById('input');
 	input.addEventListener('click', function(event) {
